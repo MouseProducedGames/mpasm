7094e814eefb46974c4ac21a539c39703f1057a8
lrgmo <lrgmo@d45ffa37-4f50-dc4c-95d2-86aaf83a6260>
2018-06-21 16:19:25 +0000

    Run call encapsulated.
    git-svn-id: file:///C/Repositories/mpasm/trunk/mpasm@88 d45ffa37-4f50-dc4c-95d2-86aaf83a6260

--- mpasm/run.h
+++ mpasm/run.h
@@ -2,18 +2,8 @@
 #define MPASM_MPASM_RUN_H
 
 #include "stdafx.h"
+
 #include <vector>
-
-#include "bytecode.h"
-#include "dtfuncs.h"
-
-#define pdt(dt) (DT)((uint8_t)DT::Ptr | (uint8_t)dt)
-#define arraydt(dt) (DT)((uint8_t)DT::Array | (uint8_t)dt)
-
-#define typecall(shorttype)\
-case DT::##shorttype##: DT##shorttype##(inst, IP, stk, SP, memadjust); break;\
-case pdt(DT::##shorttype##): DTP##shorttype##(inst, IP, stk, SP, memadjust); break;\
-case pdt(arraydt(DT::##shorttype##)): DTPA##shorttype##(inst, IP, stk, SP, memadjust); break;\
 
 void Run(
 	const std::vector<uint8_t> &inst,
@@ -21,28 +11,7 @@ void Run(
 	std::vector<byte> &stk,
 	size_t &SP,
 	size_t memadjust
-)
-{
-	while (IP < inst.size())
-	{
-		switch ((DT)inst[IP])
-		{
-		default: return;
-			typecall(F32);
-			typecall(F64);
-
-			typecall(I8);
-			typecall(I16);
-			typecall(I32);
-			typecall(I64);
-
-			typecall(UI8);
-			typecall(UI16);
-			typecall(UI32);
-			typecall(UI64);
-		}
-	}
-}
+);
 
 #endif // !MPASM_MPASM_RUN_H
 
